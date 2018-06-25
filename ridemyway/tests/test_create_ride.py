38c7f30569e6455a4735d0fa2cdf524925ed9a01
Joshua Ondieki <joshuaondieki8@gmail.com>
2018-06-25 18:31:46 +0300

    [Chore #158592616] A ride is created successfully with valid data

--- ridemyway/tests/test_create_ride.py
+++ ridemyway/tests/test_create_ride.py
@@ -19,6 +19,20 @@ class TestCreateRideAPIEndpoints(unittest.TestCase):
     def tearDown(self):
         self.context.pop()
 
+    def test_creates_a_ride_successfully_with_valid_data(self):
+        data = {
+            'data':
+                [{'departure': 'Jun 25 2018  1:30PM',
+                  'origin': 'Nairobi',
+                  'destination': 'Garissa',
+                  'cost': 350, 'vehicle_number_plate':
+                  'KBC-A21', 'capacity': 3}]}
+
+        self.response = self.client().post('/api/v1/rides', data=data)
+        result = json.loads(self.response.data.decode())
+        self.assertEqual(result['message'], "Ride created successfully")
+        self.assertEqual(self.response.status_code, 201)
+
 
 # Just incase a testing library is not used!
 if __name__ is "__main__":
