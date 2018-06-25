9c0a6aa68f29e8690e45ab8925362795641b09ff
Joshua Ondieki <joshuaondieki8@gmail.com>
2018-06-25 20:21:53 +0300

    [Chore #158592619] Test fetches all rides successfully

---
+++ ridemyway/tests/test_fetch_rides.py
@@ -0,0 +1,52 @@
+import unittest
+import json
+from ridemyway import create_app
+
+
+class TestFetchRideAPIEndpoint(unittest.TestCase):
+    """
+        Tests Fetch Ride API endpoint
+        - Ride:     '/api/v1/rides'             # GET
+        - Ride:     '/api/v1/rides/<rideId>'    # GET
+    """
+
+    def setUp(self):
+        self.app = create_app(config_name='testing')
+        self.client = self.app.test_client
+        self.headers = {'content-type': 'application/json'}
+        self.context = self.app.app_context()
+        self.context.push()
+        data = {
+            'data': [
+                {'departure': 'Jun 25 2018  1:30PM',
+                 'origin': 'Nairobi',
+                 'destination': 'Garissa',
+                 'cost': 350,
+                 'vehicle_number_plate': 'KBC-A21',
+                 'capacity': 3
+                 },
+                {'departure': 'Jun 28 2018  7:00AM',
+                 'origin': 'Garissa',
+                 'destination': 'Nairobi',
+                 'cost': 500,
+                 'vehicle_number_plate': 'KBC-A21',
+                 'capacity': 3
+                 }
+                ]
+            }
+
+        self.client().post('/api/v1/rides', data=data)
+
+    def tearDown(self):
+        self.context.pop()
+
+    def test_fetches_rides_successfully(self):
+        self.response = self.client().get('/api/v1/rides')
+        result = json.loads(self.response.data.decode())
+        self.assertEqual(result['message'], 'Rides retrieved successfully')
+        self.assertEqual(self.response.status_code, 200)
+
+
+# Just incase a testing library is not used!
+if __name__ is "__main__":
+    unittest.main()
