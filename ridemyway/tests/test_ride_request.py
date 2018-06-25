cfd16f81e7a9d4c016485ff8414167bff50075b6
Joshua Ondieki <joshuaondieki8@gmail.com>
2018-06-25 23:12:02 +0300

    [Finishes #158592730] Add info messages to tests

--- ridemyway/tests/test_ride_request.py
+++ ridemyway/tests/test_ride_request.py
@@ -42,13 +42,17 @@ class TestCreateRideRequestAPIEndpoint(unittest.TestCase):
     def test_creates_a_ride_request_successfully_with_existing_ride(self):
         self.response = self.client().post('/api/v1/rides/1/requests')
         result = json.loads(self.response.data.decode())
-        self.assertEqual(result['message'], 'Ride request created successfully')
+        self.assertEqual(result['message'],
+                         'Ride request created successfully',
+                         msg='Should successfully create a ride request\
+                          given an existing ride!')
         self.assertEqual(self.response.status_code, 201)
 
     def test_does_not_create_a_request_to_nonexistent_ride(self):
         self.response = self.client().get('/api/v1/rides/6454')
         result = json.loads(self.response.data.decode())
-        self.assertEqual(result['status'], 'failed')
+        self.assertEqual(result['status'], 'failed',
+                         msg='Should only accept requests for existing rides!')
         self.assertEqual(self.response.status_code, 404)
 
 
