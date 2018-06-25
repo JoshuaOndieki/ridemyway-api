7b3a45e8314a81436edc11bfc012f3dc956a391a
Joshua Ondieki <joshuaondieki8@gmail.com>
2018-06-26 02:26:56 +0300

    [Finishes #158607497] Setup flask-restful

--- ridemyway/resources.py
+++ ridemyway/resources.py
@@ -1,2 +1,19 @@
 from flask_restful import Resource, reqparse
+from flask_jwt_extended import (create_access_token,
+                                create_refresh_token, jwt_required,
+                                get_jwt_identity, get_raw_jwt)
 from flask import current_app as app
+
+
+class All(Resource):
+
+    def __init__(self):
+        pass
+
+    def get(self):
+        """
+            Returns:
+                All database items
+        """
+        
+        return {'message': 'all'}, 201
