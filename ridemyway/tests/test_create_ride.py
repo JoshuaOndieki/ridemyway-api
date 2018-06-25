d9915bc856a8abb434ead6783c230a8ef61311e7
Joshua Ondieki <joshuaondieki8@gmail.com>
2018-06-25 22:29:52 +0300

    Split test for invalid ride data into tests for each data point

--- ridemyway/tests/test_create_ride.py
+++ ridemyway/tests/test_create_ride.py
@@ -33,14 +33,56 @@ class TestCreateRideAPIEndpoint(unittest.TestCase):
         self.assertEqual(result['message'], "Ride created successfully")
         self.assertEqual(self.response.status_code, 201)
 
-    def test_does_not_create_ride_with_invalid_data(self):
+    def test_does_not_create_ride_with_invalid_date(self):
         data = {
             'data':
                 [{'departure': 'Not a date',
                   'origin': 'Nairobi',
                   'destination': 'Garissa',
+                  'cost': 350, 'vehicle_number_plate':
+                  'KBC-A21', 'capacity': 3}]}
+
+        self.response = self.client().post('/api/v1/rides', data=data)
+        result = json.loads(self.response.data.decode())
+        self.assertEqual(result['message'], "Invalid data given")
+        self.assertEqual(self.response.status_code, 400)
+
+    def test_does_not_create_ride_with_invalid_cost(self):
+        data = {
+            'data':
+                [{'departure': 'Jun 25 2018  1:30PM',
+                  'origin': 'Nairobi',
+                  'destination': 'Garissa',
                   'cost': '%^$', 'vehicle_number_plate':
+                  'KBC-A21', 'capacity': 3}]}
+
+        self.response = self.client().post('/api/v1/rides', data=data)
+        result = json.loads(self.response.data.decode())
+        self.assertEqual(result['message'], "Invalid data given")
+        self.assertEqual(self.response.status_code, 400)
+
+    def test_does_not_create_ride_with_invalid_capacity(self):
+        data = {
+            'data':
+                [{'departure': 'Jun 25 2018  1:30PM',
+                  'origin': 'Nairobi',
+                  'destination': 'Garissa',
+                  'cost': 350, 'vehicle_number_plate':
                   'KBC-A21', 'capacity': 'hundred'}]}
+
+        self.response = self.client().post('/api/v1/rides', data=data)
+        result = json.loads(self.response.data.decode())
+        self.assertEqual(result['message'], "Invalid data given")
+        self.assertEqual(self.response.status_code, 400)
+
+    def test_does_not_create_ride_with_invalid_vehicle_number_plate(self):
+        data = {
+            'data':
+                [{'departure': 'Jun 25 2018  1:30PM',
+                  'origin': 'Nairobi',
+                  'destination': 'Garissa',
+                  'cost': 350, 'vehicle_number_plate':
+                  2121, 'capacity': 3}]}
 
         self.response = self.client().post('/api/v1/rides', data=data)
         result = json.loads(self.response.data.decode())
