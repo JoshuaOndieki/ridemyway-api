694b83874c97479494f2873293df63296a988183
Joshua Ondieki <joshuaondieki8@gmail.com>
2018-06-25 19:32:35 +0300

    [Chore #158592616] Test does not create a ride with invalid data

--- ridemyway/tests/test_create_ride.py
+++ ridemyway/tests/test_create_ride.py
@@ -33,6 +33,20 @@ class TestCreateRideAPIEndpoints(unittest.TestCase):
         self.assertEqual(result['message'], "Ride created successfully")
         self.assertEqual(self.response.status_code, 201)
 
+    def test_does_not_create_ride_with_invalid_data(self):
+        data = {
+            'data':
+                [{'departure': 'Not a date',
+                  'origin': 'Nairobi',
+                  'destination': 'Garissa',
+                  'cost': '%^$', 'vehicle_number_plate':
+                  'KBC-A21', 'capacity': 'hundred'}]}
+
+        self.response = self.client().post('/api/v1/rides', data=data)
+        result = json.loads(self.response.data.decode())
+        self.assertEqual(result['message'], "Invalid data given")
+        self.assertEqual(self.response.status_code, 400)
+
 
 # Just incase a testing library is not used!
 if __name__ is "__main__":
