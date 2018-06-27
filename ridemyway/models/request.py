8953ced7fdda97f973cb15367aebeadd211419be
Joshua Ondieki <joshuaondieki8@gmail.com>
2018-06-27 03:14:50 +0300

    Create request model

---
+++ ridemyway/models/request.py
@@ -0,0 +1,21 @@
+class Request():
+    """
+        Creates Request objects.
+
+        **Kwargs:
+            ride_id: A unique identifier of the ride the request is
+                    being made to.
+            request_id: A unique identifier for the request.
+            status: Status of the request.
+    """
+
+    def __init__(self, **kwargs):
+        """
+            Request object initializer.
+
+            Returns:
+                Object
+        """
+        self.request_id = kwargs['request_id']
+        self.ride_id = kwargs['ride_id']
+        self.status = kwargs['status']
