667e6c82925dc20637a21eb58df9189aaedb5423
Joshua Ondieki <joshuaondieki8@gmail.com>
2018-06-28 15:27:04 +0300

    Render API docs on root url

--- ridemyway/__init__.py
+++ ridemyway/__init__.py
@@ -38,7 +38,7 @@ def create_app(config_name):
     Usage: Factory function used to setup the application instance
     :return: application instance
     """
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder='../api_docs')
     app.database = database
     app.config.from_object(config[config_name])
     app.config['BUNDLE_ERRORS'] = True
@@ -53,6 +53,11 @@ def create_app(config_name):
         jti = decrypted_token['jti']
         return jti in app.blacklist
 
+    @app.route('/')
+    def api_docs():
+        from flask import render_template
+        """ Route to the api docs"""
+        return render_template('api.html')
     # Register Blueprint here
     app.register_blueprint(v1, url_prefix="/api/v1")
 
