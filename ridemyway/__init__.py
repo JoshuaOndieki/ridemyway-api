ef816b44bce519b57bbbb61b59ad0fdb886bdc60
Joshua Ondieki <joshuaondieki8@gmail.com>
2018-06-28 08:44:50 +0300

    Remove unused jwt imports

--- ridemyway/__init__.py
+++ ridemyway/__init__.py
@@ -4,7 +4,6 @@
 
 
 from flask import Flask
-from flask_jwt_extended import JWTManager
 
 from config import config
 from .api.v1.routes import v1
@@ -48,19 +47,6 @@ def create_app(config_name):
     app.database = DATABASE
     app.config.from_object(config[config_name])
     app.config['BUNDLE_ERRORS'] = True
-    app.config['JWT_SECRET_KEY'] = 'super-secret'
-    app.config['JWT_BLACKLIST_ENABLED'] = True
-    app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
-    app.jwt = JWTManager(app)
-    app.blacklist = set()
-
-    @app.jwt.token_in_blacklist_loader
-    def check_if_token_in_blacklist(decrypted_token):
-        """
-            Check for blacklisted tokens
-        """
-        jti = decrypted_token['jti']
-        return jti in app.blacklist
 
     # Register Blueprint here
     app.register_blueprint(v1, url_prefix="/api/v1")
