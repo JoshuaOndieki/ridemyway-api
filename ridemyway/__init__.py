c1e503b06972edc6a6a0be6605c3a60755c6c7c6
Joshua Ondieki <joshuaondieki8@gmail.com>
2018-07-05 12:08:00 +0300

    Add login endpoint

--- ridemyway/__init__.py
+++ ridemyway/__init__.py
@@ -4,6 +4,7 @@
 
 
 from flask import Flask
+from flask_jwt_extended import JWTManager
 
 from config import config
 from .api.v1.routes import v1
@@ -48,6 +49,13 @@ def create_app(config_name):
     app.database = DATABASE
     app.config.from_object(config[config_name])
     app.conn = app.config['DB_CONN']
+    app.jwt = JWTManager(app)
+    app.blacklist = set()
+
+    @app.jwt.token_in_blacklist_loader
+    def check_if_token_in_blacklist(decrypted_token):
+        jti = decrypted_token['jti']
+        return jti in app.blacklist
 
     @app.route('/')
     def api_docs():
