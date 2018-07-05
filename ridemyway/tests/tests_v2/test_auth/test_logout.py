01405f1e28220cc4e0d87b27002026deafa90e30
Joshua Ondieki <joshuaondieki8@gmail.com>
2018-07-05 15:43:54 +0300

    Refactor tests

--- ridemyway/tests/tests_v2/test_auth/test_logout.py
+++ ridemyway/tests/tests_v2/test_auth/test_logout.py
@@ -5,7 +5,7 @@ import unittest
 import json
 
 from ridemyway.tests.tests_v2 import V2BaseTest
-from ridemyway.tests.tests_v2.urls import SIGNUP, LOGIN
+from ridemyway.tests.tests_v2.urls import SIGNUP, LOGIN, LOGOUT
 from ridemyway.tests.tests_v2.data import VALID_DRIVER
 
 
@@ -20,12 +20,12 @@ class TestLogout(V2BaseTest):
         self.response = self.client().post(LOGIN, data=VALID_DRIVER)
         result = json.loads(self.response.data.decode())
         access_token = result['access_token']
-        self.response = self.client().post(SIGNUP,
+        self.response = self.client().post(LOGOUT,
                                            headers=dict(
                                                Authorization="Bearer " +
                                                access_token))
         # Attempt to logout again
-        self.response = self.client().post(SIGNUP,
+        self.response = self.client().post(LOGOUT,
                                            headers=dict(
                                                Authorization="Bearer " +
                                                access_token))
